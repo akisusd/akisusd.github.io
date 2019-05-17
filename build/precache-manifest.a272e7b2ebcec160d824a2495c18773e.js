b6205c969eb41996784374e9bc97eb21f51710e9
akisusd <akos.kovacs@clever-software-solutions.com>
2019-05-17 17:07:43 +0200

    new build

---
+++ build/precache-manifest.a272e7b2ebcec160d824a2495c18773e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21b89176ef0f01c41855",
+    "url": "/akisusd/akisusd.github.io/static/js/runtime~main.3cb048b2.js"
+  },
+  {
+    "revision": "364907c75a86ee4a289d",
+    "url": "/akisusd/akisusd.github.io/static/js/main.aed0dee6.chunk.js"
+  },
+  {
+    "revision": "0590dd85e8aeb7308101",
+    "url": "/akisusd/akisusd.github.io/static/js/2.9c115357.chunk.js"
+  },
+  {
+    "revision": "364907c75a86ee4a289d",
+    "url": "/akisusd/akisusd.github.io/static/css/main.ee935c49.chunk.css"
+  },
+  {
+    "revision": "0590dd85e8aeb7308101",
+    "url": "/akisusd/akisusd.github.io/static/css/2.d256416d.chunk.css"
+  },
+  {
+    "revision": "ce58fcd80d2baf3bb55c765baca88c96",
+    "url": "/akisusd/akisusd.github.io/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/akisusd/akisusd.github.io/precache-manifest.a272e7b2ebcec160d824a2495c18773e.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute("/akisusd/akisusd.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
