4ffeed5c63699c802223421ac47b7e1a8ec039c4
akisusd <akos.kovacs@clever-software-solutions.com>
2019-07-16 13:38:33 +0200

    for api calls

--- src/classes/product.js
+++
@@ -1,8 +0,0 @@
-export class Product {
-    constructor(name, price, description, category) {
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.category = category;
-    }
-}
